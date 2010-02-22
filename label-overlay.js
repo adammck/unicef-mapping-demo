b4072c77f7edc737dfaad0665fba7b0359c7adb2
adammck <adam.mckaig@gmail.com>
2010-02-22 01:41:16 -0500

    added label overlays library
    this is entirely unrelated to this repository, i'm just using it as
    a workspace. it'll probably end up in rapidsms, or a separate repo.
    
    this provides an overlay subclass, rapidsms.maps.Label, which makes
    it really easy pin HTML fragments to the map, just like markers.

---
+++ label-overlay.js
@@ -0,0 +1,52 @@
+// vim:set noet
+
+window.rapidsms = window.rapidsms || {};
+rapidsms.maps = rapidsms.maps || {};
+
+(function(namespace) {
+	namespace.Label = function(options) {
+		this.wrapper_ = null;
+		this.content_ = null;
+		this.arrow_   = null;
+
+		this.paneName  = "overlayLayer";
+		this.className = "label";
+		this.html      = null;
+
+		this.setValues(options);
+	};
+
+	namespace.Label.prototype =
+		new google.maps.OverlayView();
+
+	namespace.Label.prototype.onAdd = function() {
+		this.wrapper_ = document.createElement("div");
+		this.wrapper_.style.position = "absolute";
+		this.wrapper_.className = "label-wrapper";
+
+		this.arrow_ = document.createElement("div");
+		this.arrow_.className = "arrow";
+		this.wrapper_.appendChild(this.arrow_);
+
+		this.content_ = document.createElement("div");
+		this.content_.className = "label " + this.className;
+		this.content_.innerHTML = this.html;
+		this.wrapper_.appendChild(this.content_);
+
+		var pane = this.getPanes()[this.paneName];
+		pane.appendChild(this.wrapper_);
+	};
+
+	namespace.Label.prototype.onRemove = function() {
+		this.wrapper_.parentNode.removeChild(this.wrapper_);
+		this.wrapper_ = null;
+		this.content_ = null;
+		this.arrow_   = null;
+	};
+
+	namespace.Label.prototype.draw = function() {
+		var position = this.getProjection().fromLatLngToDivPixel(this.position);
+		this.wrapper_.style.left = (position.x - parseInt(this.wrapper_.offsetWidth / 2)) + "px";
+		this.wrapper_.style.top  = position.y + "px";
+	};
+})(rapidsms.maps);
