8a0dcd32e1c704f18e2dd7459f1294d3135c7b69
adammck <adam.mckaig@gmail.com>
2010-03-15 18:11:42 -0400

    added modelines (vim: et ts=4 sw=4) to all files.

--- label-overlay.js
+++ label-overlay.js
@@ -1,67 +1,67 @@
-// vim:set noet
+/* vim:set et ts=4 sw=4 */
 
 window.rapidsms = window.rapidsms || {};
 rapidsms.maps = rapidsms.maps || {};
 
 (function(namespace) {
-	namespace.Label = function(options) {
-		this.wrapper_ = null;
-		this.content_ = null;
-		this.arrow_   = null;
+    namespace.Label = function(options) {
+        this.wrapper_ = null;
+        this.content_ = null;
+        this.arrow_   = null;
 
-		this.paneName  = "overlayLayer";
-		this.className = "label";
-		this.html      = null;
-		this.minZoom   = 8;
-		this.maxZoom   = 99;
-		this.setValues(options);
+        this.paneName  = "overlayLayer";
+        this.className = "label";
+        this.html      = null;
+        this.minZoom   = 8;
+        this.maxZoom   = 99;
+        this.setValues(options);
 
-		this.is_visible = function() {
-			var z = this.map.getZoom();
+        this.is_visible = function() {
+            var z = this.map.getZoom();
 
-			return (
-				(this.minZoom <= z) &&
-				(this.maxZoom >= z));
-		};
-	};
+            return (
+                (this.minZoom <= z) &&
+                (this.maxZoom >= z));
+        };
+    };
 
-	namespace.Label.prototype =
-		new google.maps.OverlayView();
+    namespace.Label.prototype =
+        new google.maps.OverlayView();
 
-	namespace.Label.prototype.onAdd = function() {
-		this.wrapper_ = document.createElement("div");
-		this.wrapper_.style.position = "absolute";
-		this.wrapper_.className = "label-wrapper";
+    namespace.Label.prototype.onAdd = function() {
+        this.wrapper_ = document.createElement("div");
+        this.wrapper_.style.position = "absolute";
+        this.wrapper_.className = "label-wrapper";
 
-		this.arrow_ = document.createElement("div");
-		this.arrow_.className = "arrow";
-		this.wrapper_.appendChild(this.arrow_);
+        this.arrow_ = document.createElement("div");
+        this.arrow_.className = "arrow";
+        this.wrapper_.appendChild(this.arrow_);
 
-		this.content_ = document.createElement("div");
-		this.content_.className = "label " + this.className;
-		this.content_.innerHTML = this.html;
-		this.wrapper_.appendChild(this.content_);
+        this.content_ = document.createElement("div");
+        this.content_.className = "label " + this.className;
+        this.content_.innerHTML = this.html;
+        this.wrapper_.appendChild(this.content_);
 
-		var pane = this.getPanes()[this.paneName];
-		pane.appendChild(this.wrapper_);
-	};
+        var pane = this.getPanes()[this.paneName];
+        pane.appendChild(this.wrapper_);
+    };
 
-	namespace.Label.prototype.onRemove = function() {
-		this.wrapper_.parentNode.removeChild(this.wrapper_);
-		this.wrapper_ = null;
-		this.content_ = null;
-		this.arrow_   = null;
-	};
+    namespace.Label.prototype.onRemove = function() {
+        this.wrapper_.parentNode.removeChild(this.wrapper_);
+        this.wrapper_ = null;
+        this.content_ = null;
+        this.arrow_   = null;
+    };
 
-	namespace.Label.prototype.draw = function() {
-		if(!this.is_visible()) {
-			this.wrapper_.style.display = "none";
-			return false;
-		}
+    namespace.Label.prototype.draw = function() {
+        if(!this.is_visible()) {
+            this.wrapper_.style.display = "none";
+            return false;
+        }
 
-		this.wrapper_.style.display = "block";
-		var position = this.getProjection().fromLatLngToDivPixel(this.position);
-		this.wrapper_.style.left = (position.x - parseInt(this.wrapper_.offsetWidth / 2)) + "px";
-		this.wrapper_.style.top  = position.y + "px";
-	};
+        this.wrapper_.style.display = "block";
+        var position = this.getProjection().fromLatLngToDivPixel(this.position);
+        this.wrapper_.style.left = (position.x - parseInt(this.wrapper_.offsetWidth / 2)) + "px";
+        this.wrapper_.style.top  = position.y + "px";
+    };
 })(rapidsms.maps);
